cfe78f59303ea89c215808e9a84619ec052e7052
sabari <mssabari503@gmail.com>
2025-04-28 17:37:54 +0530

    web page update

--- src/app/footr/footr.component.ts
+++ src/app/footr/footr.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-footr',
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrl: './footr.component.css'
 })
 export class FootrComponent {
+  isDropdownOpen = false;
+  isNavOpen = false;
 
+  toggleDropdown(event: Event) {
+    event.preventDefault();
+    console.log('Dropdown toggle clicked, current state:', this.isDropdownOpen);
+    this.isDropdownOpen = !this.isDropdownOpen;
+    console.log('Dropdown toggle new state:', this.isDropdownOpen);
+  }
+
+  onNavCheckChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.isNavOpen = target.checked;
+    console.log('Hamburger menu toggled, nav open:', this.isNavOpen);
+    if (!this.isNavOpen) {
+      this.isDropdownOpen = false; // Close dropdown when hamburger menu closes
+      console.log('Dropdown closed due to hamburger menu closing');
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.dropdown') && this.isDropdownOpen) {
+      this.isDropdownOpen = false;
+      console.log('Dropdown closed due to outside click');
+    }
+  }
 }
