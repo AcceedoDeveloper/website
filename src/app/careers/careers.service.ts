b9be02ae4e62ec32ed635a1773d360f4cef9683e
sudhar1325 <sudharsanankg5005@gmail.com>
2026-06-23 10:43:14 +0530

    career updation

--- src/app/careers/careers.service.ts
+++ src/app/careers/careers.service.ts
@@ -2,28 +2,28 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CareerData } from './careers.component'; // adjust path as needed
+import { ConfigService } from '../service/config.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CareersService {
-
-
-
-  constructor(private http: HttpClient) {}
-
- private baseUrl = 'http://localhost:3008/api';
-
-getAllCareers() {
-  return this.http.get<CareerData[]>(`${this.baseUrl}/getJobs`);
-}
-createCareer(payload: Partial<CareerData>) {
-  return this.http.post<CareerData>(`${this.baseUrl}/createJob`, payload);
-}
-updateCareer(id: string, payload: Partial<CareerData>) {
-  return this.http.put<CareerData>(`${this.baseUrl}/updateJob/${id}`, payload);
-}
-deleteCareer(id: string) {
-  return this.http.delete<void>(`${this.baseUrl}/deleteJob/${id}`);
-}
+  constructor(private http: HttpClient,private config: ConfigService) { }
+ // private baseUrl = 'http://localhost:3008/api';
+  getAllCareers() {
+    const url = this.config.getWebsiteUrl('getJobs');
+    return this.http.get<CareerData[]>(url);
+  }
+  createCareer(payload: Partial<CareerData>) {
+    const url = this.config.getWebsiteUrl('createJob');
+    return this.http.post<CareerData>(url, payload);
+  }
+  updateCareer(id: string, payload: Partial<CareerData>) {
+    const url = this.config.getWebsiteUrl('updateJob');
+    return this.http.put<CareerData>(`${url}/${id}`, payload);
+  }
+  deleteCareer(id: string) {
+    const url = this.config.getWebsiteUrl('deleteJob');
+    return this.http.delete<void>(`${url}/${id}`);
+  }
 }
